354643575432daa49f4edf495810f199a46c6227
Juan Reina <jreinaz@unal.edu.co>
2022-06-14 21:11:49 -0500

    Antlr4 Esqueleto

---
+++ matlabVisitor.js
@@ -0,0 +1,214 @@
+// Generated from matlab.g4 by ANTLR 4.10.1
+// jshint ignore: start
+import antlr4 from 'antlr4';
+
+// This class defines a complete generic visitor for a parse tree produced by matlabParser.
+
+export default class matlabVisitor extends antlr4.tree.ParseTreeVisitor {
+
+	// Visit a parse tree produced by matlabParser#primary_expression.
+	visitPrimary_expression(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#postfix_expression.
+	visitPostfix_expression(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#index_expression.
+	visitIndex_expression(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#index_expression_list.
+	visitIndex_expression_list(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#array_expression.
+	visitArray_expression(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#unary_expression.
+	visitUnary_expression(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#unary_operator.
+	visitUnary_operator(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#multiplicative_expression.
+	visitMultiplicative_expression(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#additive_expression.
+	visitAdditive_expression(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#relational_expression.
+	visitRelational_expression(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#equality_expression.
+	visitEquality_expression(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#and_expression.
+	visitAnd_expression(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#or_expression.
+	visitOr_expression(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#expression.
+	visitExpression(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#assignment_expression.
+	visitAssignment_expression(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#eostmt.
+	visitEostmt(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#statement.
+	visitStatement(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#statement_list.
+	visitStatement_list(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#identifier_list.
+	visitIdentifier_list(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#global_statement.
+	visitGlobal_statement(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#clear_statement.
+	visitClear_statement(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#expression_statement.
+	visitExpression_statement(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#assignment_statement.
+	visitAssignment_statement(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#array_element.
+	visitArray_element(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#array_list.
+	visitArray_list(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#selection_statement.
+	visitSelection_statement(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#elseif_clause.
+	visitElseif_clause(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#iteration_statement.
+	visitIteration_statement(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#jump_statement.
+	visitJump_statement(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#translation_unit.
+	visitTranslation_unit(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#func_ident_list.
+	visitFunc_ident_list(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#func_return_list.
+	visitFunc_return_list(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#function_declare_lhs.
+	visitFunction_declare_lhs(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by matlabParser#function_declare.
+	visitFunction_declare(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+
+}
